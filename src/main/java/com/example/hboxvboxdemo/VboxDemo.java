dff050b02a0341803a69144a44278579d1a1e610
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-09-29 13:20:40 -0400

    name change

--- src/main/java/com/example/hboxvboxdemo/VboxDemo.java
+++ src/main/java/com/example/hboxvboxdemo/VboxDemo.java
@@ -1,6 +1,5 @@
 package com.example.hboxvboxdemo;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +11,8 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class VboxDemo implements Initializable {
-    private final ArrayList<Button> buttonArrayList = new ArrayList<>();
-    public VBox mainVbox;
+    private final ArrayList<Button> buttonArrayList = new ArrayList<>(10);
+    public VBox root;
     public Button btn1;
 
 
@@ -21,16 +20,17 @@ public class VboxDemo implements Initializable {
     public double ButtonWidth;
     public double widthRoot;
     public double heightRoot;
-public Stage primaryStage;
+    public Stage primaryStage;
 
     @FXML
-    void insertButton(ActionEvent event) {
+    void insertButton() {
         btnCount++;
         ButtonWidth = btn1.getWidth();
-        mainVbox.setPrefWidth(ButtonWidth);
-        heightRoot = mainVbox.getHeight();
-        widthRoot = mainVbox.getWidth();
-        String buttonName = "button " + btnCount;
+        root.setPrefWidth(ButtonWidth);
+        heightRoot = root.getHeight();
+        widthRoot = root.getWidth();
+        root.setMaxHeight(300);
+        String buttonName = "Radio " + btnCount;
 
         Button buttoName = new Button();
 
@@ -38,22 +38,20 @@ public Stage primaryStage;
 
         buttoName.setText(buttonName);
 
-        mainVbox.getChildren().add(buttoName);
+        root.getChildren().add(buttoName);
 
         buttonArrayList.add(buttoName);
 
-        int amtBtns = buttonArrayList.size();
 
-        if (amtBtns >= 10) {
-            System.out.println(widthRoot);
-            this.widthRoot = widthRoot + 50;
-
+        if (btnCount >= 10) {
             primaryStage = HelloApplication.myStage;
             primaryStage.setTitle("New");
-
-            System.out.println("not working");
-
+            primaryStage.setMinWidth(primaryStage.getWidth() + btn1.getWidth());
+            System.out.println("In function");
+            btnCount = 0;
         }
+
+        // TODO: 9/29/2022 stop new buttons being added to the arraylist
 
     }
 
